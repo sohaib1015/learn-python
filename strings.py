b20087bd590011595ea16769da637529fd394557
bilalahmad <bilalshakeel590@gmail.com>
2017-03-20 19:13:16 +0500

    split,upper methods

---
+++ strings.py
@@ -0,0 +1,32 @@
+# print ('hello')
+
+# print('bilal ahmad')
+# print('ahmad')
+
+# from __future__ import print_function
+
+# print('here is the new line /n  and here is the second line')
+
+s='hello world'
+
+# print(s)
+# print(s[0])
+#
+# print(s[1:])
+#
+# print(s[:5])
+#
+# print(s[-1])
+#
+# print(s[:-1])
+#
+# print(s[::2])
+#
+# print(s[::-1])
+
+print(s.upper())
+print(s.lower())
+print(s.split('e'))
+
+print(s+ ' bilal')
+print (s*10)
