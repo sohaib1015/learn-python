69172e00ac57daef2c8628cf141803e786a970a6
bilalahmad <bilalshakeel590@gmail.com>
2017-03-22 19:18:34 +0500

    list[]

---
+++ lists.py
@@ -0,0 +1,18 @@
+list= [1, 3, 5]
+print(list)
+print(list[0])
+
+
+list.append('bilal')
+print(list)
+
+list.pop()
+print(list)
+print(list.pop(0))
+
+print(list)
+
+list.reverse()
+list.reverse()
+print(list)
+
