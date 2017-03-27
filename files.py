602d9955ed3ee2ecf1997836aecc4a99ccc97a44
bilalahmad <bilalshakeel590@gmail.com>
2017-03-28 02:21:19 +0500

    seek, read and readline

--- files.py
+++ files.py
@@ -1,4 +1,20 @@
-f=open('tuples.py')
-print(f.read())
+# Python uses file objects to interact with external files on your computer.
+# These file objects can be any sort of file you have on your computer
+#
+# read the file through
+# f.read method
+
+# This happens because you can imagine the reading "cursor" is at the end of the file after having read it.
+#  So there is nothing left to read. We can reset the "cursor" like this:
+# g.seek
+
+g = open('lists.py')
+print(g.read)
+print(g.seek(0))
+print(g.read())
+
+
+
+
 
 
