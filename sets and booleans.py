4198da9bcff5cb93dc5f7813417e5b9c524b7d8a
bilalahmad <bilalshakeel590@gmail.com>
2017-03-28 03:51:58 +0500

    k

---
+++ sets and booleans.py
@@ -0,0 +1,28 @@
+# sets are unique
+# x= set()
+# print(x)
+#
+# x.add(1)
+# print(x)
+#
+# x.add(2)
+# print(x)
+#
+# l=[1,3,5,4,2,2,3]
+# l.sort()
+# print(l)
+#
+# print(set(l))
+
+
+# booleans
+
+a = True
+print(a)
+
+print(11>2)
+
+b= None
+print(b)
+b='a'
+print(b)
