66e88f7705b2f54f51fa036cc484344cdcfbb14d
bilalahmad <bilalshakeel590@gmail.com>
2017-03-30 19:34:11 +0500

    d

---
+++ python statements/forloop.py
@@ -0,0 +1,46 @@
+#
+# a=[1,2,3,4,5]
+# for item in a:
+#     print('something')
+
+
+# b=[1,2,3,4,5]
+# for num in b:
+#     if num%2==1:
+#         print('here is odd')
+#     else:
+#         print('here is even')
+
+# b=[1,2,3,4,5]
+# list_sum=0
+# for item in b:
+#     list_sum += item
+#     print(list_sum)
+
+# s='this is string!'
+# for letters in s:
+#     print(letters)
+
+# tup=(1,2,3,4,5)
+# for t in tup:
+#     print(t)
+#
+l=[(2,4),(6,8),(6,2)]
+# for tupp in l:
+#     print(tupp)
+#
+# for (t1,t2) in l:
+#     print(t2)
+
+
+a=[1,3,2,4,4]
+print(set(a))
+
+for (t1,t2) in l:
+     print(t1+t2)
+
+
+aa={'k1':1, 'k2':2, 'k3':3}
+for item in aa:
+    print(item)
+
---
+++ python statements/if else.py
@@ -0,0 +1,30 @@
+x= 'bbb'
+if x=='bbb':
+    print('true')
+else:
+    print('false')
+
+if False:
+    print('your statement is true')
+else:
+    print('false')
+
+# nested
+
+loc='sami'
+sok='aaa'
+if loc=='ali':
+    print('success')
+elif loc =='sami':
+    print('success 100%')
+else:
+    print('not success')
+if sok =='bbb':
+    print('correct')
+else:
+    print('not correct')
+
+
+
+
+
