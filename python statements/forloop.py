9f7e9ac8fcfb140b4f78035bced7065893338041
bilalahmad <bilalshakeel590@gmail.com>
2017-04-01 03:31:42 +0500

    loops

--- python statements/forloop.py
+++ python statements/forloop.py
@@ -11,11 +11,11 @@
 #     else:
 #         print('here is even')
 
-# b=[1,2,3,4,5]
-# list_sum=0
-# for item in b:
-#     list_sum += item
-#     print(list_sum)
+b=[1,2,3,4,5]
+list_sum=0
+for item in b:
+    list_sum += item
+    print(list_sum)
 
 # s='this is string!'
 # for letters in s:
@@ -44,3 +44,11 @@ aa={'k1':1, 'k2':2, 'k3':3}
 for item in aa:
     print(item)
 
+for k,d in aa.items():
+    print (k)
+    print (d)
+
+b=[1,3,4,5,3]
+for items in b:
+    print(b[0])
+    print(b[1])
