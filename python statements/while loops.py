5791e9f976cc4c22df52e29371df63e8b9746e6d
bilalahmad <bilalshakeel590@gmail.com>
2017-04-03 18:18:47 +0500

    loops while

---
+++ python statements/while loops.py
@@ -0,0 +1,34 @@
+# x= 0
+# while x<10:
+#     print('x is currently: ',x)
+#     x +=1
+
+# x= 0
+# while x<10:
+#     print('x is currently: ',x)
+#     x+=1
+#
+# else:
+#     print('all done')
+#     x +=1
+
+
+# x=1
+# while x<10:
+#     print('x is currently: ',x)
+#     print('x is less than 10, adding 1 to x')
+#     x+=1
+#
+#     if x==3:
+#         print('x==3')
+#         break
+#
+#     else:
+#         print('continuing....')
+#         continue
+
+#
+# while True:
+#     print ('Uh Oh infinite Loop!')
+
+
