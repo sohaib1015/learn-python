ceca97822371554242992328b45a1c83649e3f90
bilalahmad <bilalshakeel590@gmail.com>
2017-04-04 19:30:48 +0500

    lisst

---
+++ python statements/list comprehension.py
@@ -0,0 +1,47 @@
+# l=[]
+# for letter in 'bilal':
+#     l.append(letter)
+# print(l)
+#
+# c=[]
+# for bletter in 'cat':
+#     c.append(bletter)
+# print(c)
+
+# square=[x**2 for x in range(0,11)]
+# print(square)
+#
+# square=[]
+# for x in range(1,10):
+#     square.append(range)
+#     print(x)
+
+# lst=[]
+# for bb in range(10):
+#     if bb %2==0:
+#         lst.append(bb)
+# print(lst)
+
+# lst=[number for number in range(11) if number %2==0]
+# print(lst)
+#
+
+# a=[1,2,3,4,5]
+# for c in a:
+#     if c%2==0:
+#         print("even numbers")
+#     else:
+#         print("odd numbers")
+
+
+# celsius =[0,1,10,1,34.5]
+# fahrenheit=[(temp * (9/5.0)+32) for temp in celsius]
+# print(fahrenheit)
+
+
+lisst=[x**2 for x in[x**2 for x in range(0,11)]]
+print(lisst)
+
+
+
+
---
+++ python statements/range.py
@@ -0,0 +1,11 @@
+start=0
+stop=20
+range(start,stop,3)
+print(range)
+
+for num in range(1,11):
+    print(num)
+
+x= range(1,4)
+type(x)
+print(type(x))
