de890e1a557f489eefed089f3f9deeb204156336
bilalahmad <bilalshakeel590@gmail.com>
2017-04-07 02:28:01 +0500

    d

---
+++ Functions/lambda function.py
@@ -0,0 +1,45 @@
+# def anonymous(num,num2):return num+num2
+# x=anonymous(1,4)
+# print(x)
+#
+
+
+# def mul(num):return num**2
+# print(mul(5))
+#
+
+
+# square=lambda num1:num1**2
+# print(square(5))
+#
+
+# even=lambda num:num%2==0
+# print(even(6))
+#
+
+# def finction_even(num1):
+#     return num1%2==0
+# print(finction_even(3))
+
+#
+# def n(nu,nu1):
+#     return nu*nu1
+# print(n(4,3))
+#
+# def add(x1,x2):
+#     return x1+x2
+# print(add(2,4))
+#
+# add=lambda x,y:x+y
+# print(add(23,43))
+
+lam=lambda first:first[0]
+print(lam('bilal'))
+
+c=lambda s:s[::-1]
+print(c('bilal'))
+
+
+
+
+
