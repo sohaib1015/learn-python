514d7659294f1d3a0cfbada6d776a649d8eebfc2
bilalahmad1994 <bilalshakeel590@gmail.com>
2017-04-07 08:15:28 +0500

    a

---
+++ Functions/functiion.py
@@ -0,0 +1,29 @@
+# def myfunction(arg1, arg2):
+#     print(arg1+arg2)
+# myfunction(1,2)
+#
+# def helloworld(name):
+#     print('my name is '+name )
+# helloworld('bilal')
+
+
+# def student(num1,num2):
+#     return num1+num2
+# x=student(1,4)
+# print(x)
+
+def prime_number(num):
+    for n in range(2,num):
+        # n is 2 and num is argument in which you have already give the argument for n that is 9
+        if num%n==0:
+            print('prime number')
+        else:
+            print('not prime number')
+prime_number(10)
+
+
+
+
+
+
+
