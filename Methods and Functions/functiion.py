bf588e7d2d181ae544d05cbe5eb0d93a797b60a1
bilalahmad1994 <bilalshakeel590@gmail.com>
2017-04-17 18:40:38 +0500

    c

--- Methods and Functions/functiion.py
+++ Methods and Functions/functiion.py
@@ -7,39 +7,51 @@
 # helloworld('bilal')
 
 
-def student(num1,num2):
-    return num1+num2
-x=student(2,5)
-print(student(4,4))
-print(x)
+# def student(num1,num2):
+#     return num1+num2
+# x=student(2,5)
+# print(student(4,4))
+# print(x)
 
 
-# def prime_number(num):
-#     for n in range(2,num):
-#         # n is 2 and num is argument in which you have already give the argument for n that is 9
-#         if num%n==0:
-#             print('prime number')
-#         else:
-#             print('not prime number')
-# prime_number(10)
-
-
-
-
-s= 'Bilal ahmad'
-def globloc(s):
-    d={"upper":0,"lower":0}
-    for c in s:
-        if c.isupper():
-            d["upper"]+=1
-        elif c.islower():
-            d["lower"]+=1
+def prime_number(num):
+    for n in range(2,num):
+        # n is 2 and num is argument in which you have already give the argument for n that is 9
+        if num%n==0:
+            print('prime number')
         else:
-            pass
-    print("on going name",s)
-    print("upper",d["upper"])
-    print("lower",d["lower"])
-globloc(s)
+            print('not prime number')
+prime_number(4)
+
+
+
+
+# s= 'Bilal ahmad'
+# def globloc(s):
+#     d={"upper":0,"lower":0}
+#     for c in s:
+#         if c.isupper():
+#             d["upper"]+=1
+#         elif c.islower():
+#             d["lower"]+=1
+#         else:
+#             pass
+#     print("on going name",s)
+#     print("upper",d["upper"])
+#     print("lower",d["lower"])
+# globloc(s)
+#
+
+
+
+
+def primenumber(num):
+    for n in range(1,num):
+        if num%n==0:
+            print("prime number")
+        else:
+            print('not prime number')
+prime_number(2)
 
 
 
---
+++ python oop/classes.py
@@ -0,0 +1,33 @@
+# class employee():
+#     pass
+#
+# emp_1=employee()
+# emp_2=employee()
+#
+# emp_1.firstname='bilal'
+# emp_1.lastname='ahmad'
+# emp_1.email='bilalshakeel590@gmail.com'
+#
+# emp_2.firstname='sami'
+# emp_2.lastname='ahmad'
+# emp_2.email='samishakeel590@gmail.com'
+#
+#
+# print(emp_1.email)
+# print(emp_2.email)
+#
+
+# def __init(self)-->is init method (e.g initialize( if you are coming from other language it is called constructor)
+# when we create methods in class it recieve first instances(arguments) automatically
+
+class employee():
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first+'.'+last+'gmail.com'
+
+emp_1=employee('bilal','ahmad',5000)
+emp_2=employee('sami','ahmad',3000)
+print(emp_1.email)
+print(emp_2.email)
