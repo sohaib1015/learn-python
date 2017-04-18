24c24c25de16997910709968a25bd202c60aeb9d
bilalahmad1994 <bilalshakeel590@gmail.com>
2017-04-18 18:24:12 +0500

    classes and methods

--- python oop/classes.py
+++ python oop/classes.py
@@ -20,21 +20,49 @@
 # def __init(self)-->is init method (e.g initialize( if you are coming from other language it is called constructor)
 # when we create methods in class it recieve first instances(arguments) automatically
 
-class employee():
-    def __init__(self,first,last,pay):
-        self.first=first
-        self.last=last
-        self.pay=pay
-        self.email=first+'.'+last+'gmail.com'
+# class employee():
+#     def __init__(self,first,last,pay):
+#         self.first=first
+#         self.last=last
+#         self.pay=pay
+#         self.email=first+'.'+last+'gmail.com'
+#
+#     def fullname(self):
+#         return '{} {}'.format(emp_1.first, emp_1.last)
+# emp_1=employee('bilal','ahmad',5000)
+# emp_2=employee('sami','ahmad',3000)
+#
+# print(emp_1.email)
+# print(emp_2.email)
+# print(emp_1.fullname())
+#
 
-    def fullname(self):
-        return '{} {}'.format(emp_1.first, emp_1.last)
-emp_1=employee('bilal','ahmad',5000)
-emp_2=employee('sami','ahmad',3000)
 
-print(emp_1.email)
-print(emp_2.email)
-print(emp_1.fullname())
+
+class employees():
+     def __init__(self,first,last,pay):
+         self.first=first
+         self.last=last
+         self.pay=pay
+         self.email=first +'.' + last + '@gmail.com'
+
+     def name(self):
+         return (self.first + self.last)
+
+emp=employees('bilal','ahmad',2000)
+# emp.name (emp is attribute that called method function)
+print(emp.name())
+# employees.name(employees is class that function and it is necessary to pass the arguments like emp)
+print(employees.name(emp))
+
+
+
+
+
+
+
+
+
 
 
 
