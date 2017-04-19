a9af36137a3f43c6ec3623e67145c46f0c2e81cd
bilalahmad <bilalshakeel590@gmail.com>
2017-04-19 21:13:35 +0500

    derivedclass

---
+++ python oop/Inheritance.py
@@ -0,0 +1,53 @@
+# class  animal:
+#     def __init__(self):
+#         print('animal created')
+#
+#     def whatname(self):
+#         print('animal')
+#
+#     def eat(self):
+#         print('eating')
+#
+# class lion(animal):
+#     def __init__(self):
+#         animal.__init__(self)
+#         print('dog created')
+#
+#     def bark(self):
+#         print('for what')
+#
+#     def jungle(self):
+#         print('jungle king')
+
+# d=lion()
+# print(d.whatname())
+# print(d.bark())
+
+
+
+
+class Animal(object):
+    def __init__(self):
+        print ("Animal created")
+
+    def whoAmI(self):
+        print ("Animal")
+
+    def eat(self):
+        print ("Eating")
+
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print ("Dog created")
+
+    def whoAmI(self):
+        print ("Dog")
+
+    def bark(self):
+        print ("Woof!")
+d=Dog()
+print(d)
+print(d.bark())
+print(d.eat())
